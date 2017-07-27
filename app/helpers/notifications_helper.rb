d536aa36a544171748f3576635f140abc8edad23
a-kubotera <akira.kubotera+github@gmail.com>
2017-07-27 20:02:07 +0900

    DIVE19_2修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,11 @@ module NotificationsHelper
   def posted_time(time)
     time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
   end
+  def chk_comment(notice)
+    @bool = notice.read
+    unless @bool
+      return "未読"
+    end
+  end
+
 end
